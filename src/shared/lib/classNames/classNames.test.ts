16e9b042591ec9d4bf822740f46cd8912f094fbb
Olexander Venger <alexvenger90@gmail.com>
2024-02-27 12:58:03 +0200

    feat: set up tests

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,19 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only one first param', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+
+    test('with additional class ands mods', () => {
+        const expectedResult = 'someClass active converted';
+
+        expect(classNames('someClass', { converted: true }, ['active'])).toBe(expectedResult);
+    });
+
+    test('with mods undefined', () => {
+        const expectedResult = 'someClass active';
+
+        expect(classNames('someClass', { converted: undefined }, ['active'])).toBe(expectedResult);
+    });
+});
--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from 'app/providers/ThemeProvider';
-import { classNames } from 'shared/lib/classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
 import { Button } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
 
