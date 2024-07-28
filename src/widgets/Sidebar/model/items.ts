d0d95c3eeb658b576ec57daacda057a3e87d6d69
oleksandrvenger <olexandervenger@gmail.com>
2024-07-28 19:42:43 +0300

    feat: add isAuth property

--- src/widgets/Sidebar/model/items.ts
+++ src/widgets/Sidebar/model/items.ts
@@ -4,10 +4,11 @@ export interface SidebarItemType {
     path: string;
     text: string;
     icon?: string;
+    authOnly?: boolean;
 }
 
 export const SidebarItemsList: SidebarItemType[] = [
     { path: RoutePath.main, text: 'Home' },
     { path: RoutePath.about, text: 'About' },
-    { path: RoutePath.profile, text: 'Profile' },
+    { path: RoutePath.profile, text: 'Profile', authOnly: true },
 ];
--- src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -1,14 +1,22 @@
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
 import { SidebarItemType } from 'widgets/Sidebar/model/items';
+import { useSelector } from 'react-redux';
+import { getUserAuthData } from 'entities/User';
 import cls from './SidebarItem.module.scss';
 
 interface SidebarItemProps {
-    item: SidebarItemType
+    item: SidebarItemType;
+
 }
 
 export const SidebarItem = ({ item }: SidebarItemProps) => {
     const { t } = useTranslation();
+    const isAuth = useSelector(getUserAuthData);
+
+    if (item.authOnly && !isAuth) {
+        return null;
+    }
 
     return (
         <AppLink
